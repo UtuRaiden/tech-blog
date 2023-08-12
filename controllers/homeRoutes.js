3ab238b0098a85de44b26775deb280bab0378992
unknown <barronpeterson56@gmail.com>
2023-08-11 19:37:08 -0500

    add views files

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { User, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Prevent non logged in users from viewing the homepage
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+        include:[
+    {
+        module:User,
+        attributes:['name'],
+    }        
+    ]
+    })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If a session exists, redirect the request to the homepage
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
